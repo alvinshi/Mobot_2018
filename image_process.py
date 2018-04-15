d5ee42600c11a766e5a844c558f1840f90d85adf
Henry <apple@henry-mac.local>
2018-04-15 09:52:18 -0400

    image segmentation for multiple path

--- image_process.py
+++ image_process.py
@@ -48,6 +48,12 @@ def get_middle(img):
     croppedImg = img[0:rowNum, midCol-colInterval:midCol+colInterval]
     return croppedImg
 
+# Dilation to expand white line after thresholding
+def dilation(img):
+    kernel = np.ones((17,17), np.uint8)
+    img_dilation = cv2.dilate(img, kernel, iterations=1)
+    return img_dilation
+
 def thresholding(img):
     #x_thresh = sobel_thresh(img, orient='x', min=30,max=150)
     #mag_thr = mag_thresh(img, sobel_kernel=3, mag_thresh=(40, 150))
@@ -58,7 +64,7 @@ def thresholding(img):
     luv_thresh = luv_select(img, thresh=(170, 255))
     thresholded = np.zeros_like(hls_thresh)
     thresholded[((hls_thresh == 1) & (lab_thresh == 1)) & (luv_thresh==1)]=255
-    return thresholded
+    return dilation(thresholded)
 
 # adaptive thresholding method
 def adaptive_thresholding(img):
@@ -67,6 +73,83 @@ def adaptive_thresholding(img):
     cv2.imshow("adaptive", adaptive_threshed)
     return adaptive_threshed
 
+# Normalize the thresholded image to a binary image
+def normalize(img):
+    normalizeImg = img.copy()
+    for row in range(0, img.shape[0]):
+        for col in range(0, img.shape[1]):
+            if img[row][col] == 255:
+                normalizeImg[row][col] = 1
+            else:
+                normalizeImg[row][col] = 0
+    return normalizeImg
+
+def get_center(coordinates):
+    index = 0
+    centers = []
+    while coordinates[index] != None:
+        sums = [0,0]
+        for i in coordinates[index]:
+            (row, col) = i
+            sums[0] = sums[0] + row
+            sums[1] = sums[1] + col
+        sums[0] = int(math.floor(sums[0] / len(coordinates[index])))
+        sums[1] = int(math.floor(sums[1] / len(coordinates[index])))
+        centers.append(tuple(sums))
+        index = index + 1
+    return centers
+
+# double raster for image segmentation
+def double_raster(imgTakein, startRow):
+    # take in binary image
+    img = normalize(imgTakein)
+    cur_label=2
+    coordinates = [None] * 50
+    eq=[]
+    for i in range(0,len(img)*len(img[0])):
+        eq.append(0)
+    for row in range(0,len(img)):
+        for col in range(0,len(img[row])):
+            if(img[row][col]==1):
+                if(row>0):
+                    up=img[row-1][col]
+                else:
+                    up=0
+                if(col>0):
+                    left=img[row,col-1]
+                else:
+                    left=0
+                if(up==0 and left==0):
+                    img[row][col]=cur_label
+                    cur_label=cur_label+1
+                elif(up!=0 and left!=0):
+                    img[row][col]=min(up,left)
+                    if(up!=left):
+                        eq[max(up,left)]=min(up,left)
+                        a=min(up,left)
+                        while(eq[a]!=0):
+                            eq[max(up,left)]=eq[a]
+                            a=eq[a]
+                elif(up==0 or left==0):
+                    img[row][col]=max(up,left)
+
+    for r in range(0,len(img)):
+        for c in range(0,len(img[r])):
+            if(img[r][c]!=0):
+                if(eq[img[r][c]]!=0):
+                    img[r][c]=eq[img[r][c]]
+                if coordinates[img[r][c]-2] == None:
+                    coordinates[img[r][c]-2] = [(c,r+startRow)]
+                else:
+                    coordinates[img[r][c]-2].append((c,r+startRow))
+
+    print("finished double raster one pic")
+    centers = get_center(coordinates)
+    print("finished get center")
+    return centers
+
+# Returns   1. Segment centors (including two different paths)
+#           2. bool path diverge state
 def row_segment_centor(img, NUM_SEGS):
     # Segment the original image into 20 segments
     numSegs = NUM_SEGS
@@ -74,7 +157,7 @@ def row_segment_centor(img, NUM_SEGS):
     numCols = img.shape[1]
     rowInterval = numRows/numSegs
     segmentCentors = [None] * numSegs
-
+    blockCenters = []
     startRow = 0
     for i in range(0, numSegs):
         imgSeg = img[startRow:startRow+rowInterval, 0:numCols]
@@ -87,10 +170,12 @@ def row_segment_centor(img, NUM_SEGS):
         else:
             rmean=int(math.floor(np.mean(coor[:,0])))
             cmean=int(math.floor(np.mean(coor[:,1])))
+
         segmentCentors[i] = (cmean, startRow+rmean)
-        startRow = startRow + rowInterval   # upddate row
-        label = "imgSeg %d" %i
-    return segmentCentors
+        startRow = startRow + rowInterval   # update row
+        blockCenters.append(double_raster(imgSegThreshed, startRow))
+
+    return segmentCentors, blockCenters
 
 def decide_way(img):
     img=cv2.GaussianBlur(img,(25,25),0)
@@ -114,16 +199,18 @@ def decide_way(img):
     return command,img
 
 def main():
-    PICTURE_FILE = './sample_pictures/WechatIMG78.jpeg'
+    PICTURE_FILE = './sample_pictures/WechatIMG89.jpeg'
     NUM_SEGS = 40
 
     img = cv2.imread(PICTURE_FILE)
     img = cv2.GaussianBlur(img,(13,13),0)
     img = get_middle(img)
-    segmentCentors = row_segment_centor(img, NUM_SEGS)
+    segmentCentors, blockCenters = row_segment_centor(img, NUM_SEGS)
     for i in range(0, NUM_SEGS):
         cv2.circle(img, segmentCentors[i], 5, (255,0,0))
-    #command, img = decide_way(img)
+        for j in range(0, len(blockCenters[i])):
+            cv2.circle(img, blockCenters[i][j], 5, (0,0,255))
+
 
     cv2.imshow('image',img)
     cv2.waitKey(0)
