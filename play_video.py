16641c0b9be9bd1dae1986e3b0c8403c21d3f1fc
Henry Zhang <hzhang0407@gmail.com>
2018-04-18 14:20:34 -0400

    coment out save in play video

--- play_video.py
+++ play_video.py
@@ -6,9 +6,9 @@ index = 0
 while(cap.isOpened()):
     ret, frame=cap.read()
     cv2.imshow('frame',frame)
-    if index % 20 == 0:
-        imageName = './sample_pictures/%d.jpg'%index
-        cv2.imwrite(imageName, frame)
+    # if index % 20 == 0:
+    #     imageName = './sample_pictures/%d.jpg'%index
+    #     cv2.imwrite(imageName, frame)
     index = index + 1
     if cv2.waitKey(10) & 0xff == ord('q'):
         break
