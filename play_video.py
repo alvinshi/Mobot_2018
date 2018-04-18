6275af61f5e96a524c1a29e514d88bfd2fe51531
Henry Zhang <hzhang0407@gmail.com>
2018-04-18 14:19:47 -0400

    sample image from the latest video

--- play_video.py
+++ play_video.py
@@ -2,12 +2,16 @@ import numpy as np
 import cv2
 
 cap=cv2.VideoCapture('output2.avi')
+index = 0
 while(cap.isOpened()):
     ret, frame=cap.read()
     cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    if index % 20 == 0:
+        imageName = './sample_pictures/%d.jpg'%index
+        cv2.imwrite(imageName, frame)
+    index = index + 1
+    if cv2.waitKey(10) & 0xff == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
