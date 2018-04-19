c83103bca6b7f7de3888b9a4b2750e7801618256
Henry Zhang <hzhang0407@gmail.com>
2018-04-19 01:59:07 -0400

    Adding decision making

--- ImageProcess.py
+++ ImageProcess.py
@@ -7,7 +7,7 @@ import time
 import Threshold as th
 
 MODE = "video"  # image or video
-NUM_SEGS = 20   # Number of row slices
+NUM_SEGS = 35  # Number of row slices
 IMG_FRACTION = 0.75  # fraction of the middle image
 
 # get the middle part of the image for image processing
@@ -24,7 +24,7 @@ def get_middle(img, fraction = 0.5):
 
 # Returns   1. Segment centors (including two different paths)
 #           2. bool whether it is at intersection state
-def row_segment_centers(img, NUM_SEGS, colOffset, CONNECTIVITY=8, AREA_THRESH=800):
+def row_segment_centers(img, NUM_SEGS, colOffset, CONNECTIVITY=8, AREA_THRESH=500):
     numSegs = NUM_SEGS
     numRows = img.shape[0]
     numCols = img.shape[1]
@@ -32,22 +32,19 @@ def row_segment_centers(img, NUM_SEGS, colOffset, CONNECTIVITY=8, AREA_THRESH=80
     startRow = 0
     centroids = []
     consecDiverge = 0
-    atIntersection = False
+    frameAtIntersection = False
     for i in range(0, numSegs):
         imgSeg = img[startRow:startRow+rowInterval, 0:numCols]
         output = cv2.connectedComponentsWithStats(imgSeg, CONNECTIVITY, cv2.CV_32S)
         labelNum = output[0]
         # check intersection
-        if (i < numSegs/2):
-            if labelNum == 2:
+        if i < numSegs/2:
+            if labelNum == 3:
                 consecDiverge += 1
             else:
                 consecDiverge = 0
-            if consecDiverge >= 4:
-                atIntersection = True
-            else:
-                atIntersection = False
-
+            if consecDiverge >= 10:
+                frameAtIntersection = True
         # get centroids
         stats = output[2]
         for j in range(1, labelNum):    # Start from 1 to ignore background label
@@ -56,28 +53,49 @@ def row_segment_centers(img, NUM_SEGS, colOffset, CONNECTIVITY=8, AREA_THRESH=80
                 y = int(output[3][j][1])+startRow
                 centroids.append((x, y))
         startRow += rowInterval
-
-    if (atIntersection):
-        print("at intersection")
-    else:
-        print("not at intersection")
-
-    return centroids
+    return centroids, frameAtIntersection
 
 def image_process(img, NUM_SEGS, IMG_FRACTION):
     img = cv2.GaussianBlur(img,(13,13),0)
     imgThreshed = th.thresholding(img)
     imgMiddle, colOffset = get_middle(imgThreshed, IMG_FRACTION)
-    centroids = row_segment_centers(imgMiddle, NUM_SEGS, colOffset)
-
+    centroids, frameAtIntersection = row_segment_centers(imgMiddle, NUM_SEGS, colOffset)
+    # Draw all
     for i in range(0,len(centroids)):
         cv2.circle(img, centroids[i], 5, (255,0,0))
-
     cv2.imshow('imageThreshed',imgThreshed)
     cv2.imshow('image', img)
+    return centroids, atIntersection
+
+def get_right_centroids(centroids):
+    pass
+
+def get_left_centroids(centroids):
+    pass
+
+def get_middle_centroids(centroids):
+    pass
+
+# Called in the robot main loop
+# Takes in last N frames of intersection state (to get rid of noise)
+# Takes in preferred side if at intersection
+# Takes in centroids
+# return command and current "atIntersection" state
+def get_command(pastStates, preferredSide, centroids, frameAtIntersection):
+    for i in range(0, len(pastStates)):
+        if not pastStates[i]:
+            command = get_middle_centroids(centroids)
+            return command, frameAtIntersection
+    if preferredSide == "left":
+        command = get_left_centroids(centroids)
+        return command, frameAtIntersection
+    elif preferredSide == "right":
+        command = get_right_centroids(centroids)
+        return command, frameAtIntersection
+    else:
+        pass
 
 def main():
-
     PICTURE_FILE = './sample_pictures/200.jpg'
     VIDEO_FILE = './videos/output.avi'
     global NUM_SEGS
@@ -93,7 +111,7 @@ def main():
         while True:
             ret, frame = vid.read()
             image_process(frame, NUM_SEGS, IMG_FRACTION)
-            if cv2.waitKey(1) & 0xff == ord('q'):
+            if cv2.waitKey(10) & 0xff == ord('q'):
                 break
 
 
